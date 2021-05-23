eb93ef10586e8694601be9f89d7d6fec22dc9bd5
Dhyata Garg <dhyatagarg@gmail.com>
2021-05-23 22:16:56 +0530

    Initial commit.

---
+++ src/store/cart-slice.js
@@ -0,0 +1,49 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const cartSlice = createSlice({
+  name: "cart",
+  initialState: {
+    items: [],
+    totalQuantity: 0,
+    changed: false,
+  },
+  reducers: {
+    replaceCart(state, actions) {
+      state.totalQuantity = actions.payload.totalQuantity;
+      state.items = actions.payload.items;
+    },
+    addItemToCart(state, action) {
+      const newItem = action.payload;
+      const existingItem = state.items.find((item) => item.id === newItem.id);
+      if (!existingItem) {
+        state.items.push({
+          id: newItem.id,
+          price: newItem.price,
+          quantity: 1,
+          totalPrice: newItem.price,
+          name: newItem.title,
+        });
+      } else {
+        existingItem.quantity++;
+        existingItem.totalPrice += existingItem.price;
+      }
+      state.changed = true;
+      state.totalQuantity++;
+    },
+    removeItemFromCart(state, action) {
+      const id = action.payload;
+      const existingItem = state.items.find((item) => item.id === id);
+      if (existingItem.quantity === 1) {
+        state.items = state.items.filter((item) => item.id !== id);
+      } else {
+        existingItem.quantity--;
+        existingItem.totalPrice -= existingItem.price;
+      }
+      state.changed = true;
+      state.totalQuantity--;
+    },
+  },
+});
+
+export const cartActions = cartSlice.actions;
+export default cartSlice;
